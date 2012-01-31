5bf069d1ff24735e6f6b67955e20fa2502605341
Sidney San Martín <s@sidneysm.com>
2012-01-30 23:03:08 -0500

    Various content updates, updated templating system

--- js/resume.js
+++ js/resume.js
@@ -1,85 +1,61 @@
 (function(){
-	var $loadError = $('#loadError'),
-		résuméView = $.haml.placeholder((function(){
-			var converter = new Showdown.converter(),
-				template = ['#resume',
-					['#info',
-						['%p#name', {key: 'name'}],
-						['%p#contact', {key:'contact', children:['%span', {key:'method'}, ': ', ['%a', {href:{key:'link'}}, {key:'value'}]]}]
-					],
-					{key: 'experience', conditional: [
-						['%h1', "Experience"],
-						['%ul#experience', {key: 'experience', children:[ '%li',
-							['%h2', {key:'name'}, {key: 'time', conditional:[' ', ['%span.times', {key:'time', handler:stringForTime}]]}],
-							{key: 'projects', conditional:['%ul.projects', {key: 'projects', children:[ '%li',
-								['%h3', {key: 'name'}, {key: 'time', conditional:[' ', ['%span.times', {key:'time', handler:stringForTime}]]}],
-								{key: 'description', handler:parseMarkdown}
-							]}]}
-						]}]
-					]},
-					{key: 'competencies', conditional:[
-						['%h1', 'Competencies'],
-						[ '%ul#competencies', {key: 'competencies', children:[ '%li',
-							['%h2', {key: 'category'}],
-							['%ul', {key: 'values', children:[
-								'%li', {key: ''}
-							]}]
-						]}]
-					]},
-					{key: 'certifications', conditional:[
-						['%h1', 'Certifications'],
-						['%ul#certifications', {key: 'certifications', children:[ '%li',
-							['%a', {href: {key:'url'}}, {key: 'name'}]
-						]}]
-					]},
-					{key: 'affiliations', conditional:[
-						['%h1', 'Affiliations'],
-						['%ul#affiliations', {key: 'affiliations', children:[ '%li',
-							['%div', ['%a', {href: {key:'url'}}, {key: 'name'}]], ['%div.location', {key: 'location'}]
-						]}]
-					]},
-					{key: 'conferences', conditional:[
-						['%h1', 'Conferences'],
-						['%ul#conferences', {key: 'conferences', children:[ '%li',
-							['%div', ['%a', {href: {key:'url'}}, {key: 'name'}], {key: 'extra', conditional:[' ', ['%span', '(', {key:'extra'}, ')']]}, {key: 'year', conditional:[' ', ['%span.times', {key:'year'}]]}], ['%div.location', {key: 'location'}]
-						]}]
-					]},
-					{key: 'mailingLists', conditional:[
-						['%h1', 'Mailing Lists'],
-						['%ul#mailingLists', {key: 'mailingLists', children:[
-							['%li', ['%div', ['%a', {href: {key:'url'}}, {key: 'name'}]]]
-						]}]
-					]}
-				];
-			function parseMarkdown(){
-				return $(converter.makeHtml(this));
-			}
-			function stringForTime(){
-				return this && this.start ? ('(' + this.start + (this.start !== this.end ? '-' + (this.end || 'present') : '') + ')') : '';
-			}
-			return function(résumé){
-				if (!$.isPlainObject(résumé)) {
-					return document.createTextNode('');
-				};
-		
-				if (résumé.name) {
-					document.title = résumé.name + ' – Résumé'
-				}
-		
-				return stencil(template, résumé);
-		}})()),
-		résuméSource = $('script:last').attr('data-src');
+	var template = (function(){
+		function parseMarkdown(markdown){
+			return $(converter.makeHtml(markdown)).get().reduce(function(df, el){ df.appendChild(el); return df }, document.createDocumentFragment());
+		}
+		function stringForTime(time){
+			return time && time.start ? ('(' + time.start + (time.start !== time.end ? '-' + (time.end || 'present') : '') + ')') : '';
+		}
+		var converter = new Showdown.converter(),
+			template = ['#resume',
+				['#info',
+					['%p#name', {$key: 'name'}],
+					['%p#contact', {$key:'contact', $children:['%span', {$key:'method'}, ': ', ['%a', {href:{$key:'link'}}, {$key:'value'}]]}]
+				],
+				{$test: {$key: 'experience'}, $if: [
+					['%h1', "Experience"],
+					['%ul#experience', {$key: 'experience', $children:[ '%li',
+						['%h2', {$key:'name'}, {$test: { $key: 'time' }, $if:[' ', ['%span.times', {$key:'time', $handler:stringForTime}]]}],
+						{$test: { $key: 'projects' }, $if:['%ul.projects', {$key: 'projects', $children:[ '%li',
+							['%h3', {$key: 'name'}, {$test: { $key: 'time' }, $if:[' ', ['%span.times', {$key:'time', $handler:stringForTime}]]}],
+							{$key: 'description', $handler:parseMarkdown}
+						]}]}
+					]}]
+				]},
+				{$test: { $key: 'environments' }, $if:[
+					['%h1', 'Languages and Tools'],
+					['%ul#environments', {$key: 'environments', $children:[
+						'%li', {$key: ''}
+					]}]
+				]},
+				{$test: { $key: 'meetups' }, $if:[
+					['%h1', 'Favorite Meetups'],
+					['%ul#meetups', {$key: 'meetups', $children:[ '%li',
+						['%div', ['%a', {href: {$key:'url'}}, {$key: 'name'}]]
+					]}]
+				]},
+				{$test: { $key: 'conferences' }, $if:[
+					['%h1', 'Conferences'],
+					['%ul#conferences', {$key: 'conferences', $children:[ '%li',
+						['%div', ['%a', {href: {$key:'url'}}, {$key: 'name'}], {$test: { $key: 'extra' }, $if:[' ', ['%span', '(', {$key:'extra'}, ')']]}, {$test: { $key: 'year' }, $if:[' ', ['%span.times', {$key:'year'}]]}], ['%div.location', {$key: 'location'}]
+					]}]
+				]}
+			];
+		return template;
+	})();
 		
 	function showError(){
-		$loadError.addClass('visible');
+		$('#loadError').addClass('visible');
 	}
-	$(document.body).append(résuméView.inject());
 	$.ajax({
-		url: résuméSource,
+		url: $('script:last').attr('data-src'),
 		dataType: 'json',
 		success: function(data){
 			if (data) {
-				résuméView.update(data);
+				document.title = data.name + ' – Résumé'
+				$(document.body).append(function(){
+					return haj(stencil(template, data));
+				});
 			} else {
 				showError();
 			}
